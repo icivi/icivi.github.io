18a9eed93290777f34d306125d82a48baf519c19
shatatoy <quloff@gmail.com>
2022-06-28 16:06:42 +0300

    tek

--- about/index.php
+++ about/index.php
@@ -44,7 +44,7 @@
                                 <span class="subheading">Tatoy</span>
                             <h2 class="mb-4">НИИ Коллективного Искусственного Интеллекта</h2>                        </div>
                             <div>
-                                <p>On her way she met a copy. The copy warned the Little Blind Text, that where it came from it would have been rewritten a thousand times and everything that was left from its origin would be the word "and" and the Little Blind Text should turn around and return to its own, safe country. But nothing the copy said could convince her and so it didn’t take long until a few insidious Copy Writers ambushed her, made her drunk with Longe and Parole and dragged her into their agency, where they abused her for theiOOOOOOOOOOO.</p>
+                                <p> </p>
                             </div>
                         </div>
                     </div>
