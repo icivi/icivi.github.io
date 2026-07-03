f74b4846eef124a461e43004d8b5c0a77f9dda94
icivi <zh777alexandr@gmail.com>
2026-07-04 02:22:38 +0300

    тест2

--- server_web_cli.bat
+++ server_web_cli.bat
@@ -3,5 +3,5 @@ echo Запуск встроенного Веб сервера Cli
 echo Зайти на сервер в Хроме http://localhost:8000/
 
 
-cd c:\sites\icivi.github.io\ 
+cd h:\github\icivi.github.io\ 
 c:\php\php.exe -S localhost:8000
